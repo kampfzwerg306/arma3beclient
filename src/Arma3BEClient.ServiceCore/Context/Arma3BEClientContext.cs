e5bb8c4a65e069fd220623514548b0aabe7446dd
Artem Muradov <tym32167@gmail.com>
2015-05-10 15:34:31 +0500

    Migrations

---
+++ src/Arma3BEClient.ServiceCore/Context/Arma3BEClientContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using Arma3BEClient.Libs.ModelCompact;
+
+namespace Arma3BEClient.Libs.Context
+{
+    public class Arma3BeClientServiceContext : DbContext
+    {
+        public DbSet<ChatLog> ChatLog { get; set; }
+        public DbSet<Note> Comments { get; set; }
+        public DbSet<Player> Player { get; set; }
+        public DbSet<ServerInfo> ServerInfo { get; set; }
+
+        public DbSet<Settings> Settings { get; set; }
+
+        public DbSet<Ban> Bans { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<PlayerHistory> PlayerHistory { get; set; }
+
+        public Arma3BeClientServiceContext()
+            : base("name=DefaultConnection")
+        {
+
+        }
+
+        static Arma3BeClientServiceContext()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (var db = new Arma3BeClientServiceContext())
+                db.Database.Initialize(false);
+        }
+    }
+
+
+    class DbInitializer : CreateDatabaseIfNotExists<Arma3BeClientContext>
+    {
+        public DbInitializer()
+        {
+
+        }
+    }
+}
