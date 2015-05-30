904da780ba7cd41451a613bf4f6e3fffe8a4482a
Artem Muradov <tym32167@gmail.com>
2015-05-30 21:54:11 +0500

    removed unnececcary files

--- src/Arma3BEClient.Web/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Arma3BEClient.Web.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- src/Arma3BEClient.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arma3BEClient.Web.Global" Language="C#" %>
