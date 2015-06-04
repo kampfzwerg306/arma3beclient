a0c594a54c1e9ff20bab68a9788114e48d8aaa03
Artem Muradov <tym32167@gmail.com>
2015-06-05 00:23:05 +0500

    Added topshelf, maybe it will fine for win services

---
+++ src/Arma3BEService.Core/BackendRunner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+using Arma3BEService.Service;
+using Topshelf;
+
+namespace Arma3BEService.Core
+{
+    public class BackendRunner : ServiceControl
+    {
+        private ServiceHost serviceHost;
+
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("start");
+            serviceHost = new ServiceHost(typeof(TestService));
+            serviceHost.Open();
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Console.WriteLine("stop");
+            serviceHost.Close();
+            serviceHost = null;
+            return true;
+        }
+    }
+}
---
+++ src/Arma3BEService.Core/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Topshelf;
+
+namespace Arma3BEService.Core
+{
+    static class Program
+    {
+        static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<BackendRunner>();
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
+                
+                x.SetDescription("Arma3BEService");
+                x.SetDisplayName("Arma3BEService");
+                x.SetServiceName("Arma3BEService");
+            });
+        }
+    }
+}
